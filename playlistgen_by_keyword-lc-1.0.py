335f66945e1ae1c6a105a14c999ca6057e9a71c6
chankonabe <livingtribunal@gmail.com>
2017-11-12 11:55:29 -0800

    Added code and gitignore

---
+++ playlistgen_by_keyword-lc-1.0.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+
+import sys
+import os
+import httplib
+import httplib2
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import run
+
+# CLIENT_SECRETS_FILE, name of a file containing the OAuth 2.0 information for
+# this application, including client_id and client_secret. You can acquire an
+# ID/secret pair from the API Access tab on the Google APIs Console
+#   http://code.google.com/apis/console#access
+# For more information about using OAuth2 to access Google APIs, please visit:
+#   https://developers.google.com/accounts/docs/OAuth2
+# For more information about the client_secrets.json file format, please visit:
+#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+# Please ensure that you have enabled the YouTube Data API for your project.
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# A limited OAuth 2 access scope that allows for uploading files, but not other
+# types of account access.
+
+YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+# Helpful message to display if the CLIENT_SECRETS_FILE is missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+
+   %s
+
+with information from the APIs Console
+https://code.google.com/apis/console#access
+
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+
+def get_authenticated_service():
+  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_READ_WRITE_SCOPE,
+    message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+  storage = Storage("%s-oauth2.json" % sys.argv[0])
+  credentials = storage.get()
+
+  if credentials is None or credentials.invalid:
+    credentials = run(flow, storage)
+
+  return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    http=credentials.authorize(httplib2.Http()))
+
+
+def youtube_playlist_from_keyword(options):
+  youtube = get_authenticated_service()
+  
+  # Call the search.list method to retrieve results matching the specified
+  # query term.
+  search_response = youtube.search().list(
+    q=options.q,
+    part="id,snippet",
+    forMine="true",
+    type="video",
+    maxResults=options.max_results
+  ).execute()
+
+  videos = []
+  channels = []
+  playlists = []
+
+  # Add each result to the appropriate list, and then display the lists of
+  # matching videos, channels, and playlists.
+  for search_result in search_response.get("items", []):
+    if search_result["id"]["kind"] == "youtube#video":
+      videos.append("%s (%s)" % (search_result["snippet"]["title"],
+                                 search_result["id"]["videoId"]))
+    elif search_result["id"]["kind"] == "youtube#channel":
+      channels.append("%s (%s)" % (search_result["snippet"]["title"],
+                                   search_result["id"]["channelId"]))
+    elif search_result["id"]["kind"] == "youtube#playlist":
+      playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+                                    search_result["id"]["playlistId"]))
+
+  print "Videos:\n", "\n".join(videos), "\n"
+  print "Channels:\n", "\n".join(channels), "\n"
+  print "Playlists:\n", "\n".join(playlists), "\n" 
+
+  playlists_insert_response = youtube.playlists().insert(
+    part="snippet,status",
+	body=dict(
+	  snippet=dict(
+	    title=options.q,
+		description="A private playlist created with the YouTube API v3"
+	  ),
+	  status=dict(
+	    privacyStatus="private"
+	  )
+    )
+  ).execute()
+
+  playlist_id = playlists_insert_response["id"]
+  print "New playlist id: %s" % playlist_id
+	
+  for search_result in search_response.get("items", []):
+  	if search_result["id"]["kind"] == "youtube#video":
+	  playlistitems_insert_response = youtube.playlistItems().insert(
+		part="snippet",
+		body=dict(
+		  snippet=dict(
+		    playlistId=playlist_id,
+		    resourceId=search_result["id"],
+		  )
+		)
+	  ).execute()
+	  print "Added video %s to playlist %s " % (search_result["id"]["videoId"],playlist_id) 
+	  			
+if __name__ == "__main__":
+  argparser.add_argument("--q", help="Search term", default="Google")
+  argparser.add_argument("--max-results", help="Max results", default=25)
+  args = argparser.parse_args()
+
+  try:
+    youtube_playlist_from_keyword(args)
+  except HttpError, e:
+    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
